066bd148ef6828dcdc6c90d715f8385a2c2249a6
Vladislav Leohin <93376866+SempaiSSD@users.noreply.github.com>
2021-11-02 17:44:52 +0200

    Update script.js

--- homework_3/script.js
+++ homework_3/script.js
@@ -1,4 +1,19 @@
-//Задание 2
+//Задание 1 (На отрезке чисел, посчитать числа кратные 6 и их сумму)
+let n1 = +(prompt('Введите число 1'));
+let n2 = +(prompt('Введите число 2'));
+
+let result1 = 0; //kolvo % 6
+let result2 = 0;// summa
+for (let i = n1; i < n2; i++) {
+	if (i % 6 === 0) {
+		console.log(i);
+		result1++;
+		result2 = result2 + i;
+	}
+}
+alert(`Ответ: ${result1}, ${result2}`);
+
+//Задание 2 (Мини-игра "Угадай число")
 let min = 0;
 let max = 100;
 trys = 0;
