5677da08c7f914514c5f75f2da8e11d855b52bcd
Vladislav Leohin <93376866+SempaiSSD@users.noreply.github.com>
2021-11-04 17:53:17 +0200

    Update script.js

--- homework_5/script.js
+++ homework_5/script.js
@@ -1,30 +1,31 @@
 //Напишите функцию random(min, max), 
 //которая будет возвращать случае целое число от min до max
-// function random(min, max) {
-// 	let result;
-// 	if (isNaN(min) && isNaN(max)) {
-// 		alert('Неверное число');
-// 		return;
-// 	}
-// 	return Math.floor((max - min) * Math.random() + min);
-// }
-// function call_random() {
-// 	min = +(prompt('min'));
-// 	max = +(prompt('max'));
-// 	for (let i = 0; i < 5; i++) {
-// 		console.log(random(min, max));
-// 	}
-// }
+function random(min, max) {
+	let result;
+	return Math.floor((max - min) * Math.random() + min);
+}
+function call_random() {
+	min = +(prompt('min'));
+	max = +(prompt('max'));
+	for (let i = 0; i < 5; i++) {
+		console.log(random(min, max));
+	}
+}
 
 
 
 
-// Написать функцию count_symbol( symbol, string ), 
-// которая подсчитает количество указанного символа symbol,
-// в строке string.
-// Функция должна быть регистронезависимой и вернуть число
+//Напишите функцию get_max( arr ), которая находит максимальное значение в заданном массиве.
+function get_max(arr) {
+	let max_num = arr[0];
 
+	for (let i in arr) {
+		if (arr[i] > max_num) max_num = arr[i];
+	}
+	return max_num;
+}
 
-function count_symbol(symbol, string) {
-	
+function call_get_max() {
+	let array = [10, 20, -30, -22, -5, 40, 24, 30];
+	alert(get_max(array));
 }
