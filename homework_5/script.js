4c24362149d73442f2db9cdcd1c3fb2fd4293be2
Vladislav Leohin <93376866+SempaiSSD@users.noreply.github.com>
2021-11-04 17:38:50 +0200

    Add files via upload

---
+++ homework_5/script.js
@@ -0,0 +1,30 @@
+//Напишите функцию random(min, max), 
+//которая будет возвращать случае целое число от min до max
+// function random(min, max) {
+// 	let result;
+// 	if (isNaN(min) && isNaN(max)) {
+// 		alert('Неверное число');
+// 		return;
+// 	}
+// 	return Math.floor((max - min) * Math.random() + min);
+// }
+// function call_random() {
+// 	min = +(prompt('min'));
+// 	max = +(prompt('max'));
+// 	for (let i = 0; i < 5; i++) {
+// 		console.log(random(min, max));
+// 	}
+// }
+
+
+
+
+// Написать функцию count_symbol( symbol, string ), 
+// которая подсчитает количество указанного символа symbol,
+// в строке string.
+// Функция должна быть регистронезависимой и вернуть число
+
+
+function count_symbol(symbol, string) {
+	
+}
