2fd526b1f6c45c99404149e249b54bb2519916ce
Vladislav Leohin <93376866+SempaiSSD@users.noreply.github.com>
2021-11-13 16:43:55 +0200

    Update script.js

--- homework_4/script.js
+++ homework_4/script.js
@@ -1,9 +1,61 @@
+// Задача № 1 Напишите функцию get_min(arr), которая находит минимальное значение в заданном массиве.
+
+let array = [-7, -3, 0, 3, 7];
+let arr = [];
+function get_min(arr) {
+	arr = Math.min(...array);
+	console.log(arr);
+	return arr;
+}
+get_min(arr);
+
+
+
+// Задача № 2 Создайте массив с четными числами в диапазоне от 500 до 888 включительно.
+//Вариант 1
+let arr = [];
+for (let i = 500; i <= 888; i++) {
+	if (i % 2 == 0) {
+		arr.push(i);
+	}
+}
+console.log(arr);
+
+//Вариант 2 Функция
+function arrLenght() {
+	let arr = [];
+	for (let i = 500; i <= 888; i++) {
+		if (i % 2 == 0) {
+			arr.push(i);
+		}
+	}
+	return arr;
+}
+console.log(arrLenght())
+
+
+
+
 //Задание 3 (Развернуть слово)
+
+//Вариант 1 Топ варик)))))Самый лучший)
 let arr = prompt('Введите слово', '')
 arr = arr.split('');
-console.log(arr);
 arr = arr.reverse('');
-console.log(arr)
 arr = arr.join('');
 alert(arr);
+
+//Вариант 2 Функция
+let word = prompt('Введите слово', '');
+function reverse(reverse) {
+	if (isNaN(word)) {
+		return reverse.split('').reverse().join('');
+	} else { console.error('Введите корректное число') }
+}
+alert(reverse(word));
+
+//Вариант 3 Самый простой
+let word = prompt('Введите слово', '');
+word = word.split('').reverse().join('');
+alert(word);
 
